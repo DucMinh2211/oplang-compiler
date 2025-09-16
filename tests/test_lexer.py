b2e674426e2058094a925c3a1e26e614ad2a74a2
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-09-16 18:47:07 +0700

    implement 100 lexer tests in tests/test_lexer.py

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -83,3 +83,478 @@ def test_010():
     source = "+ - * / \\ % == != < <= > >= && || ! := ^ new . ( ) [ ] { } , ; :"
     expected = "+,-,*,/,\\,%,==,!=,<,<=,>,>=,&&,||,!,:=,^,new,.,(,),[,],{,},,,;,:,EOF"
     assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_011():
+    source = """
+        Class A
+        """
+    expected = "Class,A,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_012():
+    source = "int b, c, d"
+    expected = "int,b,,,c,,,d,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_013():
+    """Test line comment"""
+    source = "# this is a line comment"
+    expected = "EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_014():
+    """Test block comment"""
+    source = "/* this is a block comment */"
+    expected = "EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_015():
+    """Test unterminated block comment"""
+    source = "/* */"
+    expected = "EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_016():
+    """Test string with all valid escapes"""
+    source = '"\\b\\f\\r\\n\\t\\"\\\\"'
+    expected = "\\b\\f\\r\\n\\t\\\"\\\\,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_017():
+    """Test special characters ~ and &"""
+    source = "~ &"
+    expected = "~,&,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_018():
+    """Test invalid float starting with ."""
+    source = ".123"
+    expected = ".,123,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_019():
+    """Test invalid float ending with e"""
+    source = "123e"
+    expected = "123,e,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_020():
+    """Test another invalid character"""
+    source = "?"
+    expected = "Error Token ?"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Auto-generated tests from 21 to 100
+
+# Identifiers
+def test_021():
+    source = "_test"
+    expected = "_test,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_022():
+    source = "test_case"
+    expected = "test_case,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_023():
+    source = "Test1"
+    expected = "Test1,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_024():
+    source = "_1"
+    expected = "_1,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_025():
+    source = "t1_"
+    expected = "t1_,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Integers
+def test_026():
+    source = "1234567890"
+    expected = "1234567890,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_027():
+    source = "007"
+    expected = "007,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Floats
+def test_028():
+    source = "1.23"
+    expected = "1.23,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_029():
+    source = "1.e3"
+    expected = "1.e3,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_030():
+    source = "1.2E-3"
+    expected = "1.2E-3,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Strings
+def test_031():
+    source = '"a string"'
+    expected = "a string,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_032():
+    source = '"str with space"'
+    expected = "str with space,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_033():
+    source = '"\\n"'
+    expected = "\\n,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_034():
+    source = '"\\t"'
+    expected = "\\t,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_035():
+    source = '"\\""'
+    expected = '\\",EOF'
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Errors
+def test_036():
+    source = '"unclosed'
+    expected = "Unclosed String: unclosed"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_037():
+    source = '"illegal \\a"'
+    expected = "Illegal Escape In String: illegal \\a"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_038():
+    source = "#"
+    expected = "EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_039():
+    source = "/**/"
+    expected = "EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_040():
+    source = """
+        /* 
+        multi line comments
+        */
+    """
+    expected = "EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Keywords
+def test_041():
+    source = "boolean"
+    expected = "boolean,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_042():
+    source = "break"
+    expected = "break,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_043():
+    source = "continue"
+    expected = "continue,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_044():
+    source = "do"
+    expected = "do,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_045():
+    source = "downto"
+    expected = "downto,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_046():
+    source = "else"
+    expected = "else,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_047():
+    source = "extends"
+    expected = "extends,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_048():
+    source = "false"
+    expected = "false,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_049():
+    source = "final"
+    expected = "final,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_050():
+    source = "float"
+    expected = "float,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_051():
+    source = "for"
+    expected = "for,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_052():
+    source = "if"
+    expected = "if,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_053():
+    source = "int"
+    expected = "int,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_054():
+    source = "new"
+    expected = "new,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_055():
+    source = "nil"
+    expected = "nil,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_056():
+    source = "return"
+    expected = "return,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_057():
+    source = "static"
+    expected = "static,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_058():
+    source = "string"
+    expected = "string,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_059():
+    source = "then"
+    expected = "then,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_060():
+    source = "this"
+    expected = "this,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_061():
+    source = "to"
+    expected = "to,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_062():
+    source = "true"
+    expected = "true,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_063():
+    source = "void"
+    expected = "void,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Operators
+def test_064():
+    source = "+"
+    expected = "+,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_065():
+    source = "-"
+    expected = "-,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_066():
+    source = "*"
+    expected = "*,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_067():
+    source = "/"
+    expected = "/,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_068():
+    source = "\\"
+    expected = "\\,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_069():
+    source = "%"
+    expected = "%,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_070():
+    source = "=="
+    expected = "==,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_071():
+    source = "!="
+    expected = "!=,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_072():
+    source = "<"
+    expected = "<,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_073():
+    source = ">"
+    expected = ">,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_074():
+    source = "<="
+    expected = "<=,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_075():
+    source = ">="
+    expected = ">=,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_076():
+    source = "&&"
+    expected = "&&,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_077():
+    source = "||"
+    expected = "||,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_078():
+    source = "!"
+    expected = "!,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_079():
+    source = ":="
+    expected = ":=,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_080():
+    source = "^"
+    expected = "^,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Separators
+def test_081():
+    source = "("
+    expected = "(,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_082():
+    source = ")"
+    expected = "),EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_083():
+    source = "["
+    expected = "[,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_084():
+    source = "]"
+    expected = "],EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_085():
+    source = "{"
+    expected = "{,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_086():
+    source = "}"
+    expected = "},EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_087():
+    source = ";"
+    expected = ";,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_088():
+    source = ":"
+    expected = ":,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_089():
+    source = "."
+    expected = ".,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_090():
+    source = ","
+    expected = ",,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+# Combinations
+def test_091():
+    source = "a := b + c"
+    expected = "a,:=,b,+,c,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_092():
+    source = "if (a > b) then c := a"
+    expected = "if,(,a,>,b,),then,c,:=,a,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_093():
+    source = "for i := 1 to 10 do io.writeInt(i)"
+    expected = "for,i,:=,1,to,10,do,io,.,writeInt,(,i,),EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_094():
+    source = "return this.x"
+    expected = "return,this,.,x,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_095():
+    source = "obj := new MyClass()"
+    expected = "obj,:=,new,MyClass,(,),EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_096():
+    source = "arr[0] := 1"
+    expected = "arr,[,0,],:=,1,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_097():
+    source = "a.b.c"
+    expected = "a,.,b,.,c,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_098():
+    source = "a && b || c"
+    expected = "a,&&,b,||,c,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_099():
+    source = "-a + +b"
+    expected = "-,a,+,+,b,EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
+
+def test_100():
+    source = "a := {1,2,3}"
+    expected = "a,:=,{,1,,,2,,,3,},EOF"
+    assert Tokenizer(source).get_tokens_as_string() == expected
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -112,3 +112,492 @@ def test_011():
     source = """class Test { int x = 1; """  # Thiếu dấu }
     expected = "Error on line 1 col 24: <EOF>"
     assert Parser(source).parse() == expected
+
+def test_012():
+    """Test class inheritance"""
+    source = """class A {} class B extends A {} """
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_013():
+    """Test multiple attribute declarations"""
+    source = """class Test { int a, b; float c; }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_014():
+    """Test constructor declaration"""
+    source = """class Test { Test() {} }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_015():
+    """Test destructor declaration"""
+    source = """class Test { ~Test() {} }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_016():
+    """Test copy constructor"""
+    source = """class Test { Test(Test other) {} }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_017():
+    """Test reference parameter"""
+    source = """class Test { void foo(int & a) {} }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_018():
+    """Test reference return type"""
+    source = """class Test { int & foo() { return x; } }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_019():
+    """Test break and continue statements"""
+    source = """class Test { static void main() { for i := 1 to 10 do { break; continue; } } }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_020():
+    """Test member access and method invocation"""
+    source = """class Test { int x; void foo() { this.x := self.foo(); } }"""
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+# More parser tests from 21 to 100
+
+def test_021():
+    """Test empty class"""
+    source = "class Empty {}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_022():
+    """Test multiple classes"""
+    source = "class A {} class B {}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_023():
+    """Test static attribute"""
+    source = "class Test { static int x; }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_024():
+    """Test static final attribute"""
+    source = "class Test { static final int x = 1; }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_025():
+    """Test method returning an array"""
+    source = "class Test { int[5] foo() {} }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_026():
+    """Test method with array parameter"""
+    source = "class Test { void foo(int[5] arr) {} }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_027():
+    """Test block statement with variable declarations"""
+    source = "class Test { void main() { int x; float y; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_028():
+    """Test nested block statements"""
+    source = "class Test { void main() { { int x; } } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_029():
+    """Test arithmetic expressions with precedence"""
+    source = "class Test { void main() { x := 1 + 2 * 3; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_030():
+    """Test boolean expressions"""
+    source = "class Test { void main() { flag := a && b || !c; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_031():
+    """Test relational expressions"""
+    source = "class Test { void main() { flag := a > b == c < d; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_032():
+    """Test object creation with parameters"""
+    source = "class Test { void main() { obj := new Test(1, 2); } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_033():
+    """Test static method call"""
+    source = "class Test { void main() { Math.abs(-1); } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_034():
+    """Test if without else"""
+    source = "class Test { void main() { if (true) then {} } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_035():
+    """Test empty for loop"""
+    source = "class Test { void main() { for i := 1 to 1 do {} } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_036():
+    """Test return statement with expression"""
+    source = "class Test { int main() { return 0; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_037():
+    """Test empty return statement"""
+    source = "class Test { void main() { return; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_038():
+    """Test unary minus"""
+    source = "class Test { void main() { x := -y; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_039():
+    """Test array literal"""
+    source = "class Test { void main() { arr := {1,2,3}; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_040():
+    """Test this keyword"""
+    source = "class Test { int x; void foo() { this.x := 1; } }"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+# Error tests
+def test_041():
+    """Error: missing semicolon after attribute"""
+    source = "class Test { int x }"
+    expected = "Error on line 1 col 18: }"
+    assert Parser(source).parse() == expected
+
+def test_042():
+    """Error: missing class name"""
+    source = "class {} "
+    expected = "Error on line 1 col 6: {"
+    assert Parser(source).parse() == expected
+
+def test_043():
+    """Error: invalid inheritance"""
+    source = "class A extends {} "
+    expected = "Error on line 1 col 16: {"
+    assert Parser(source).parse() == expected
+
+def test_044():
+    """Error: missing parameter type"""
+    source = "class Test { void foo(a) {} }"
+    expected = "Error on line 1 col 22: a"
+    assert Parser(source).parse() == expected
+
+def test_045():
+    """Error: missing `then` in if statement"""
+    source = "class Test { void main() { if (true) {} } }"
+    expected = "Error on line 1 col 34: {"
+    assert Parser(source).parse() == expected
+
+def test_046():
+    """Error: missing `do` in for statement"""
+    source = "class Test { void main() { for i := 1 to 10 {} } }"
+    expected = "Error on line 1 col 41: {"
+    assert Parser(source).parse() == expected
+
+def test_047():
+    """Error: invalid assignment operator"""
+    source = "class Test { void main() { x = 1; } }"
+    expected = "Error on line 1 col 28: ="
+    assert Parser(source).parse() == expected
+
+def test_048():
+    """Error: return in constructor"""
+    source = "class Test { Test() { return; } }"
+    expected = "Error on line 1 col 23: return"
+    assert Parser(source).parse() == expected
+
+def test_049():
+    """Error: return in destructor"""
+    source = "class Test { ~Test() { return; } }"
+    expected = "Error on line 1 col 24: return"
+    assert Parser(source).parse() == expected
+
+def test_050():
+    """Error: break outside loop"""
+    source = "class Test { void main() { break; } }"
+    expected = "success" # Semantic check, not parser
+    assert Parser(source).parse() == expected
+
+# Auto-generated simple tests 51-100
+
+def test_051():
+    source = "class T {int x;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_052():
+    source = "class T {float x;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_053():
+    source = "class T {boolean x;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_054():
+    source = "class T {string x;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_055():
+    source = "class T {int[1] x;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_056():
+    source = "class T {void f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_057():
+    source = "class T {int f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_058():
+    source = "class T {float f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_059():
+    source = "class T {boolean f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_060():
+    source = "class T {string f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_061():
+    source = "class T {int[1] f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_062():
+    source = "class T {void f(int a){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_063():
+    source = "class T {void f(int a; float b){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_064():
+    source = "class T {void f(int & a){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_065():
+    source = "class T {int & f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_066():
+    source = "class T {static int x;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_067():
+    source = "class T {final int x = 1;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_068():
+    source = "class T {static final int x = 1;}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_069():
+    source = "class T {static void f(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_070():
+    source = "class T {T(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_071():
+    source = "class T {~T(){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_072():
+    source = "class T {T(T t){}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_073():
+    source = "class T {void f(){a:=1;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_074():
+    source = "class T {void f(){if true then {}}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_075():
+    source = "class T {void f(){if true then {} else {}}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_076():
+    source = "class T {void f(){for i:=1 to 2 do {}}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_077():
+    source = "class T {void f(){for i:=2 downto 1 do {}}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_078():
+    source = "class T {void f(){break;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_079():
+    source = "class T {void f(){continue;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_080():
+    source = "class T {void f(){return;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_081():
+    source = "class T {int f(){return 1;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_082():
+    source = "class T {void f(){T.f();}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_083():
+    source = "class T {void f(){this.f();}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_084():
+    source = "class T {void f(){a.b[1] := 1;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_085():
+    source = "class T {void f(){a := new T();}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_086():
+    source = "class T {void f(){a := -b;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_087():
+    source = "class T {void f(){a := !b;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_088():
+    source = "class T {void f(){a := b+c*d;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_089():
+    source = "class T {void f(){a := b&&c||d;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_090():
+    source = "class T {void f(){a := b==c;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_091():
+    source = "class T {void f(){a := b^c;}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_092():
+    source = "class T {void f(){a := {1,2};}}"
+    expected = "success"
+    assert Parser(source).parse() == expected
+
+def test_093():
+    source = "class T{int x=1;}"
+    expected = "Error on line 1 col 12: ="
+    assert Parser(source).parse() == expected
+
+def test_094():
+    source = "class T{int f(a){}}"
+    expected = "Error on line 1 col 14: a"
+    assert Parser(source).parse() == expected
+
+def test_095():
+    source = "class T{void f(){a=1;}}"
+    expected = "Error on line 1 col 18: ="
+    assert Parser(source).parse() == expected
+
+def test_096():
+    source = "class T{void f(){if(a>b){}}}"
+    expected = "Error on line 1 col 24: {"
+    assert Parser(source).parse() == expected
+
+def test_097():
+    source = "class T{void f(){for i:=1 to 10 {}}}"
+    expected = "Error on line 1 col 32: {"
+    assert Parser(source).parse() == expected
+
+def test_098():
+    source = "class T{void f(){return 1}}"
+    expected = "Error on line 1 col 27: }"
+    assert Parser(source).parse() == expected
+
+def test_099():
+    source = "class T extends A, B{}"
+    expected = "Error on line 1 col 17: ,"
+    assert Parser(source).parse() == expected
+
+def test_100():
+    source = "class T{ f() {} }"
+    expected = "Error on line 1 col 9: f"
+    assert Parser(source).parse() == expected
